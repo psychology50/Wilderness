cd753e10100cdbd7fc5a9bee07c119ea3b57928d
Yang JaeSeo <qud1251@gmail.com>
2021-12-23 01:52:03 +0900

    1.0.1-alpha

---
+++ js/Letter.js
@@ -0,0 +1,8 @@
+function open_letter() {
+    document.getElementsByClassName("letter-close")[0].style.display = 'none'
+    document.getElementsByClassName("letter-open")[0].style.display = 'block'
+}
+function go_rtan() {
+    alert('헉, 이걸 어케 찾았지');
+    window.location.href='https://psychology50.github.io/index/I_can_do_it/';
+}
